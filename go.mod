33c96c62b4f559a687054e7b695859eb39c318b2
abhishek-singh-codes <asingh19082000@gmail.com>
2026-01-15 13:12:10 +0530

    Added redis client config, and set point method

--- go.mod
+++ go.mod
@@ -30,7 +30,9 @@ require (
 	github.com/aws/smithy-go v1.24.0 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -68,6 +70,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1
+	github.com/redis/go-redis/v9 v9.17.2
 	golang.org/x/text v0.27.0 // indirect
 	gorm.io/driver/mysql v1.6.0
 )
---
+++ internals/util/tail38/client.go
@@ -0,0 +1,59 @@
+package tail38
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"sync"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type GeoClient interface {
+	SetPoint(
+		ctx context.Context,
+		collection string,
+		id string,
+		lat float32,
+		long float32,
+	) error
+}
+
+// this time I am using Tail38
+type geoClient struct {
+	rdb    *redis.Client
+	logger *slog.Logger
+}
+
+var (
+	once     sync.Once
+	instance GeoClient
+)
+
+// this func will initialize/ return the GeoClient interface
+func InitGeoClient(
+	host string,
+	port int,
+	password string,
+	logger *slog.Logger,
+) GeoClient {
+	once.Do(func() {
+		addr := fmt.Sprintf("%s:%d", host, port)
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr:         addr,
+			Password:     password,
+			PoolSize:     30,
+			MinIdleConns: 10,
+		})
+
+		instance = &geoClient{
+			rdb:    rdb,
+			logger: logger,
+		}
+
+		logger.Info("Tail38 Initialized", "addr", addr)
+	})
+
+	return instance
+}
